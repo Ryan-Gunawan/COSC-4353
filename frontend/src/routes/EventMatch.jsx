0b06c1cec1202a066b718164d08290e2f0383295
Ryan-Gunawan <ryan.liang2@gmail.com>
2024-09-20 11:48:06 -0500

    Footer Stuff

--- frontend/src/routes/EventMatch.jsx
+++ frontend/src/routes/EventMatch.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from "../components/Navbar/Navbar"
+import Footer from '../components/Footer/Footer';
 
 const PeopleEventMatcher = () => {
  // Hardcoded people details
@@ -124,18 +125,7 @@ const events = [
           </li>
         ))}
       </ul>
-      <footer className="footer">
-        <div className="footer-content">
-          <p>&copy; 2024 VolunteerMatch. All rights reserved.</p>
-          <p>Don&apos;t contact us: info@volunteermatch.org</p>
-          <p>Don&apos;t follow us on social media:</p>
-          <div className="social-links">
-            <a href="#">Facebook</a>
-            <a href="#">Twitter</a>
-            <a href="#">Instagram</a>
-          </div>
-          </div>
-          </footer>
+      <Footer />
     </div>
   );
 };
