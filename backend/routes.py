53427084420c78f90599022bcb5043ac5acb4446
fojo <jonuma9402@gmail.com>
2024-10-06 22:19:13 -0500

    Added more login/register functionality

--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,14 @@
 from app import app, db
-from flask import request, jsonify
+from flask import Flask, request, jsonify, session, redirect, url_for
+import json
 from models import User, Event
 import re
+import os
+
+app.secret_key = 'secretkey'
+USER_FILE = 'dummy/users.json'
+
+### Login and Registration Routes and Functions ###
 
 # Get all users
 # Query all users from db and return them as a list
@@ -11,18 +18,58 @@ def get_users():
     result = [user.to_json() for user in users]
     return jsonify(result), 200
 
-# to view
+# to view register route
 @app.route("/api/register", methods = ["GET"])
 def register_users():
     return "Hello, welcome to register"
 
+# Loads user data from json file. Simulates loading from a database for testing
+def load_users():
+    if not os.path.exists(USER_FILE):
+        return {"users": []} # if no file, return empty string
+    with open(USER_FILE, 'r') as f:
+        return json.load(f) # returns user json object as dictionary
+
+# Saves updated users.json
+def save_users(data):
+    with open(USER_FILE, 'w') as f:
+        json.dump(data, f, indent=4)
+
+# Function to get user by email
+def get_user_by_email(email):
+    data = load_users()
+    for user in data['users']:
+        if user['email'].lower() == email.lower(): # ignore case sensitivity
+            return user
+    return None
+
+# Function to add new user
+def add_user(email, password):
+    data = load_users()
+    new_user_id = len(data['users']) + 1 # increment id
+    new_user = {
+        "id": new_user_id,
+        "email": email,
+        "password": password
+    }
+    data['users'].append(new_user)
+    save_users(data)
+    return new_user
+
+# Return the logged in user's ID
+def get_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id:
+        return user_id
+    else:
+        return None
+
 # Functions to validate registration inputs
 def validate_email(email):
     email_regex = r'^[^\s@]+@[^\s@]+\.[^\s@]+$'
-    # Once we add database, add check that email is not already registered
     return re.match(email_regex, email) is not None
 
-
+# Ensure password requirements are met
 def validate_password(password):
     return len(password) > 0 and len(password) <= 128
 
@@ -30,13 +77,8 @@ def validate_password(password):
 def validate_login_email(email):
     valid = True
     email_regex = r'^[^\s@]+@[^\s@]+\.[^\s@]+$'
-
     if re.match(email_regex, email) is not None:
         valid = False
-
-    # if email not in database:
-        # valid = False
-
     return valid
 
 # Register validation route
@@ -45,41 +87,67 @@ def register():
     data = request.get_json()
     email = data.get("email")
     password = data.get("password")
+    user = get_user_by_email(email)
+
+    # Check if user already exists
+    if user is not None:
+        return jsonify({"msg": "An account with this email already exists"}), 400
 
     # validate email and password
     if not validate_email(email):
         return jsonify({"msg": "Invalid email"}), 400
-
     if not validate_password(password):
         return jsonify({"msg": "Invalid password"}), 400
 
-    # if validations pass return success response
+    # if validations pass add user and return success response
+    add_user(email, password)
     return jsonify({"msg": "Registration successful"}), 200
 
+# Test login function
 @app.route("/api/login", methods = ["POST"])
 def login():
     data = request.get_json()
     email = data.get("email")
     password = data.get("password")
 
-    test_email = "test@gmail.com"
-    test_pass = "password"
+    # get user corresponding to email input
+    user = get_user_by_email(email)
 
+    # Checks if user exists and if email matches password
+    if user is not None:
+        if password == user['password']:
+            session['user_id'] = user['id']
+            return jsonify({"success": True, "msg": "Login successful"}), 200
 
-    if (email == test_email and password == test_pass):
-        return jsonify({"success": True, "msg": "Login successful"}), 200
-    else:
-        return jsonify({"success": False, "msg": "Invalid email or password"}), 401
+    return jsonify({"success": False, "msg": "Invalid email or password"}), 401
 
-    # if email not in db
-    # return jsonify({"success": False, "msg": "Email not found"}), 404
-
-    # if password matches email:
-    #     return jsonify({"success": True, "msg": "Login successful"}), 200
-    # else:
-    #     return jsonify({"success": False, "msg": "Invalid password"}), 401
-
-
+# To view login route
 @app.route("/api/login", methods = ["GET"])
 def login_users():
     return "Hello, welcome to login"
+
+
+### Notification route and functions ###
+
+# Load notifs from json file
+def load_notifications():
+    with open('dummy/notifications.json', 'r') as file:
+        return json.load(file)
+
+def save_notifications(notifications):
+    with open('dummy/notifications.json', 'f') as file:
+        json.dump(notifications, file, indent=4)
+
+# User notifications page route
+@app.route('/notifications/<user_id>', methods=['GET'])
+def get_notifications(user_id):
+    notifications = load_notifications()
+    return jsonify(notifications.get(user_id, []))
+
+def delete_notification(user_id, notification_id):
+    notifications  = load_notifications()
+    user_notifications = notifications.get(user_id, [])
+    notifications[user_id] = [n for n in user_notifications if str(n['id']) != notification_id]
+    save_notifications(notifications)
+
+
--- frontend/src/components/LoginRegister/LoginRegister.jsx
+++ frontend/src/components/LoginRegister/LoginRegister.jsx
@@ -1,145 +1,171 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './LoginRegister.css';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./LoginRegister.css";
 import { FaLock, FaEnvelope } from "react-icons/fa";
 
 const LoginRegister = () => {
+  const [action, setAction] = useState("");
 
-    const [action, setAction] = useState('');
+  const registerLink = () => {
+    setAction(" register");
+  };
 
-    const registerLink = () => {
-        setAction(' register');
-    };
+  const loginLink = () => {
+    setAction(" login");
+  };
 
-    const loginLink = () => {
-        setAction(' login');
-    };
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [inputs, setInputs] = useState({
-        email: "",
-        password: ""
-    });
+  const navigate = useNavigate(); // initialize useNavigate to redirect on submit
 
-    const navigate = useNavigate(); // initialize useNavigate to redirect on submit
+  const handleRegisterSubmit = async (e) => {
+    e.preventDefault(); // prevents page reloading
 
-    const handleRegisterSubmit = async (e) => {
-        e.preventDefault(); // prevents page reloading
+    try {
+      const response = await fetch("http://localhost:5000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      });
 
-        try {
-            const response = await fetch("http://localhost:5000/api/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(inputs),
-            });
+      const result = await response.json();
+      if (response.status === 200) {
+        alert(result.msg); // show success msg
+        //alert("Registered successfully");
+        navigate("/userprofile");
+      }
+      else {
+        alert(result.msg) // show error msg
+      }
+    } catch (error) {
+      alert("A register error has occurred");
+    }
+  };
 
-            const result = await response.json();
-            alert("Registered successfully");
-            navigate("/userprofile");
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault(); // prevents page reloading
 
-        } catch (error) {
-            alert("An error has occurred");
+    try {
+      const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      });
 
-        }
-    };
+      // Gets result from backend. If email and password are valid go to home otherwise send error msg
+      const result = await response.json();
+      if (result.success) {
+        navigate("/home");
+        alert("Login successful");
+      } else {
+        alert(result.msg);
+      }
+    } catch (error) {
+      alert("A login error has occurred");
+    }
+  };
 
-
-    const handleLoginSubmit = async (e) => {
-        e.preventDefault(); // prevents page reloading
-
-        try {
-            const response = await fetch("http://localhost:5000/api/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(inputs),
-            });
-
-            // Gets result from backend. If email and password are valid go to home otherwise send error msg
-            const result = await response.json();
-            if (result.success) {
-                navigate("/home");
-                alert("Login successful");
-            } else {
-                alert(result.msg);
-            }
-
-        } catch (error) {
-            alert("An error has occurred");
-
-        }
-    };
-
-    return (
-        <div className="loginRegisterComponent">
-            <div className={`container${action}`}>
-                <div className="form-box login">
-                    <form action="" onSubmit={handleLoginSubmit}>
-                        <h1>Login</h1>
-                        <div className="input-box">
-                            <input type="email"
-                                maxLength="254"
-                                placeholder="Email"
-                                value={inputs.email}
-                                onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-                                required />
-                            <FaEnvelope className="icon" />
-                        </div>
-                        <div className="input-box">
-                            <input type="password"
-                                maxLength="128"
-                                placeholder="Password"
-                                value={inputs.password}
-                                onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-                                required />
-                            <FaLock className="icon" />
-                        </div>
-
-                        <button type="submit">Log in</button>
-
-                        <div className="register-link">
-                            <p>Don&apos;t have an account? <a href="#" onClick={registerLink} className="signup">Register</a></p>
-                        </div>
-                    </form>
-                </div>
-
-                <div className="form-box register">
-                    <form action='' onSubmit={handleRegisterSubmit}>
-                        <h1>Register</h1>
-                        <div className="input-box">
-                            <input type="email"
-                                maxLength="254"
-                                placeholder="Email"
-                                value={inputs.email}
-                                onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-                                required />
-                            <FaEnvelope className="icon" />
-                        </div>
-                        <div className="input-box">
-                            <input type="password"
-                                maxLength="128"
-                                placeholder="Password"
-                                value={inputs.password}
-                                onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-                                required />
-                            <FaLock className="icon" />
-                        </div>
-
-                        <div className="terms">
-                            <p>By signing up, you agree to our Terms.</p>
-                        </div>
-
-                        <button type="submit">Register</button>
-
-                        <div className="register-link">
-                            <p>Already have an account? <a href="#" onClick={loginLink} className="signup">Log In</a></p>
-                        </div>
-                    </form>
-                </div>
+  return (
+    <div className="loginRegisterComponent">
+      <div className={`container${action}`}>
+        <div className="form-box login">
+          <form action="" onSubmit={handleLoginSubmit}>
+            <h1>Login</h1>
+            <div className="input-box">
+              <input
+                type="email"
+                maxLength="254"
+                placeholder="Email"
+                value={inputs.email}
+                onChange={(e) =>
+                  setInputs({ ...inputs, email: e.target.value })
+                }
+                required
+              />
+              <FaEnvelope className="icon" />
             </div>
+            <div className="input-box">
+              <input
+                type="password"
+                maxLength="128"
+                placeholder="Password"
+                value={inputs.password}
+                onChange={(e) =>
+                  setInputs({ ...inputs, password: e.target.value })
+                }
+                required
+              />
+              <FaLock className="icon" />
+            </div>
+
+            <button type="submit">Log in</button>
+
+            <div className="register-link">
+              <p>
+                Don&apos;t have an account?{" "}
+                <a href="#" onClick={registerLink} className="signup">
+                  Register
+                </a>
+              </p>
+            </div>
+          </form>
         </div>
-    );
+
+        <div className="form-box register">
+          <form action="" onSubmit={handleRegisterSubmit}>
+            <h1>Register</h1>
+            <div className="input-box">
+              <input
+                type="email"
+                maxLength="254"
+                placeholder="Email"
+                value={inputs.email}
+                onChange={(e) =>
+                  setInputs({ ...inputs, email: e.target.value })
+                }
+                required
+              />
+              <FaEnvelope className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                maxLength="128"
+                placeholder="Password"
+                value={inputs.password}
+                onChange={(e) =>
+                  setInputs({ ...inputs, password: e.target.value })
+                }
+                required
+              />
+              <FaLock className="icon" />
+            </div>
+
+            <div className="terms">
+              <p>By signing up, you agree to our Terms.</p>
+            </div>
+
+            <button type="submit">Register</button>
+
+            <div className="register-link">
+              <p>
+                Already have an account?{" "}
+                <a href="#" onClick={loginLink} className="signup">
+                  Log In
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default LoginRegister;
