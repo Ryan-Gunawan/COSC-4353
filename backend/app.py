40257940eb1dd0301af0eeff27fb1e5b6c9ce3bd
fojo <jonuma9402@gmail.com>
2024-10-10 19:10:22 -0500

    Fixed session cookies not sending

--- backend/app.py
+++ backend/app.py
@@ -16,12 +16,17 @@ app.config['SQLALCHEMY_BINDS'] = {
     'events': 'sqlite:///events.db'
 }
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SESSION_COOKIE_SAMESITE'] = 'None' # required for cross-site requests. When using diff ports
-app.config['SESSION_COOKIE_SECURE'] = False # True if using HTTPS
-app.config['SECRET_KEY'] = 'secretkey' # required for sessions
-#app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
-#app.config['SESSION_TYPE'] = 'filesystem' # use filesystem-based sessions
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax' # Allows cookies to be sent during local dev
+app.config['SESSION_COOKIE_SECURE'] = False #  Allows cookies to be sent over HTTP without security
 
+# In production use these settings:
+# app.config['SESSION_COOKIE_SAMESITE'] = 'None' # required for cross-site requests. When using diff ports
+# app.config['SESSION_COOKIE_SECURE'] = True # True if using HTTPS
+
+# Other optional settings I was messing with. I don't think we need these, but just in case I'll keep them here for a bit
+# app.config['SECRET_KEY'] = 'secretkey' # required for sessions
+# app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
+# app.config['SESSION_TYPE'] = 'filesystem' # use filesystem-based sessions
 # Session(app)
 
 db = SQLAlchemy(app)
