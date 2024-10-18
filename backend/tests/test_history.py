2cc6b9a80a4690d0e11c3c428216e8cd3d7c3b13
Duyen Dang <duyendang@MacBook-Air-87.local>
2024-10-18 18:49:31 -0500

    Fix test_history fail cases

--- backend/tests/test_history.py
+++ backend/tests/test_history.py
@@ -26,7 +26,7 @@ class TestUserInfo(unittest.TestCase):
             # Expected volunteer history data
             expected_data = [{"assignedUsers":[],"date":"2024-10-15","description":"Join us for a day of insightful talks and networking with industry leaders in technology.","id":1,"location":"San Francisco, CA","name":"Tech Conference 2024"},
                             {"assignedUsers":[],"date":"2024-09-25","description":"A weekend filled with live music performances from top artists around the world.","id":2,"location":"Austin, TX","name":"Music Festival"},
-                            {"assignedUsers":[],"date":"2024-10-08T16:33:02","description":"$3.50 an hour","id":4,"location":"Houston","name":"Helping Teddy with his project","skills":["problemsolver"],"urgency":"3"}]
+                            {"assignedUsers":[],"date":"2024-10-08T16:33:02","description":"$3.50 an hour","id":4,"location":"Houston","name":"Helping Teddy with his project","skills":["Problem Solver"],"urgency":"3"}]
 
             # Assert that the returned data matches the expected event data
             self.assertEqual(data, expected_data)
